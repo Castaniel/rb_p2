31c24a196eec0aa16eb1aa10972f4ace86a44143
Marek <yurchylyak@gmail.com>
2018-04-03 15:03:55 +0300

    rb_p2

---
+++ index.rb
@@ -0,0 +1,94 @@
+# a = [1, 3, 5, 6, 9]
+# b = [1..2, 3...5, 1..5]
+# c = [[1..2], [5,4], [5]]
+# x = 1
+# y = 5
+# a = [x*2, x-y, x**y]
+# puts %w(I love Ruby)
+# a = [1, 3, 5, 2, 5, 2, 7, 4, 7, 4, 23, 53, 23, 12, 13]
+# puts a[0], a[-15], a[-1], a[a.size-1]
+# a[0] = 5
+#puts a[0]
+# a[17] = 5
+# puts a
+# x = ['a', 'b', 'c', 'd', 'e']
+# puts x[0,2], x[0..1], x[-2, 2], x[-2..-1]
+# a = [1, 2]
+# a[2, 0] = [3, 4, 5]
+# a[2..5] = [3, 4, 5]
+# a[-2] = [6]
+# puts a
+# a = [1, 3, 5]
+# b = [2, 3, 5]
+# c = a | b
+# c = a & b
+# puts a - b
+# a = []
+# puts a << 5
+# a = [2] * 10
+# puts a
+# a = [1, 1, 2, 2, 3, 3, 4]
+# b = [5, 5, 4, 4, 3, 3, 2]
+# puts a|b, b|a
+# puts a&b, b&a
+# a = ['sd', 'qwr', 'asfdasd']
+# puts a.any? { |i| i.length > 4 }
+# a.map { |i| i[-1] = '?' }
+# puts a
+# a = [1, 3, 4, 5, 6]
+# a[2] = []
+# a.each { |i| puts i }
+# a = []
+# puts a.empty?
+# puts a[0]
+# a = [[1], [2], [4]]
+# puts a.flatten
+# a = ['cat', 'dog', 'beaver']
+# puts a.any? { |i| i.include?('cat') }
+# a = [1, 2, 4, 5, 76, 7, 4, 2, 23, 543]
+# puts a.select { |i| i.even? }
+# puts a.uniq
+# a = Hash.new
+# a['first'] = 1
+# a['second'] = 2
+# a['third'] = 3
+# b = {'one' => 1, 'two' => 2, 'three' => 3}
+# puts b, a
+# a = 0..10
+# b = 0...10
+# puts a.include?(5), b.include?(10)
+# a = ('a'..'z')
+# a.step(2) { |i| print "#{i}"}
+# puts a.to_a
+# :a = 5
+# a = 'symbol to be'
+# puts a.to_sym
+# o = nil
+# puts o.nil?
+# puts false == nil?
+# a = 0.0 if false
+# puts a
+# puts b
+# PI = 3.14
+# puts PI
+# x = 1
+# x += 5
+# puts x
+# a, b, c = 1, 2, 3
+# puts a, b, c
+# a = b = 1
+# puts a, b
+# x = 1
+# x += 2
+# x -= 4
+# x *= 3
+# x /= 5
+# a = Array.new
+# b = Array.new(3)
+# c = Array.new(4, 1)
+# d = Array.new(c)
+# e = Array.new(5) { |i| i % 2 }
+# puts e
+
+
+
